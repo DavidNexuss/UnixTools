4388c32eabcca77c0f70fe0d24d9a00510c45dcd
davidnexus <dgtgar01@gmail.com>
2021-02-20 17:49:20 +0100

    Fixes exec

--- examples/exec.cc
+++ examples/exec.cc
@@ -6,5 +6,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int fd = execute_pipe(execute_pipe(0,{"cat","/proc/cpuinfo"}),{"grep","MHz"});
+    int fd2 = execute_pipe_line(0,{
+                {"cat","/proc/cpuinfo"},{"grep","MHz"},{"grep","200"}
+            });
     cpy(fd,1);
+    cpy(fd2,1);
 }
--- exec/exec.h
+++ exec/exec.h
@@ -1,14 +1,52 @@
 #pragma once
 #include <vector>
-#include <string>
-#include <signal.h>
-using namespace std;
+#include <unistd.h>
+#include <iostream>
 
-//EXECVP
-vector<string> split(const string& line);
-void execute(const vector<string>& args);
-int execute_pipe(int fd,const vector<string>& args);
-void send_notification(const string& title,const string& message);
+static void execute(const std::vector<std::string>& args)
+{
+    int n = args.size();
+    char* argv[n + 1];
 
-//SIGNALS
-void set_signal_handler(int signal,struct sigaction* sa);
+    for(int i = 0; i < n; i++) argv[i] = (char*)args[i].c_str();
+
+    argv[n] = NULL;
+    execvp(argv[0],argv);
+    perror("execvp");
+    exit(27);
+}
+
+static int execute_pipe(int fd,const std::vector<std::string>& args)
+{
+    int pipe_fd[2];
+    pipe(pipe_fd);
+
+    int f = fork();
+    if (f == 0)
+    {
+        if (fd != 0)
+        {
+            dup2(fd,0);
+            close(fd);
+        }
+
+        dup2(pipe_fd[1],1);
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        execute(args);
+    }
+
+    //close(fd) Hauria de funcionar 
+    close(pipe_fd[1]);
+    return pipe_fd[0];
+}
+
+static int execute_pipe_line(int input_fd,const std::vector<std::vector<std::string>>& args)
+{
+    int current_input = input_fd;
+    for(int i = 0; i < args.size(); i++)
+    {
+        current_input = execute_pipe(current_input,args[i]);
+    }
+    return current_input;
+}
